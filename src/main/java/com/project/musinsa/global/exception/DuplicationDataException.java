a346a6e6e086ad93bfbcede972951b2a935224ff
dkf4929 <sng4929@naver.com>
2024-07-10 18:37:31 +0900

    feat: custom exception 처리

---
+++ src/main/java/com/project/musinsa/global/exception/DuplicationDataException.java
@@ -0,0 +1,7 @@
+package com.project.musinsa.global.exception;
+
+public class DuplicationDataException extends RuntimeException {
+    public DuplicationDataException(String message) {
+        super(message);
+    }
+}
