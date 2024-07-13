ac39d2861acb59447a870cac2114162ddb955b51
dkf4929 <sng4929@naver.com>
2024-07-13 16:36:55 +0900

    refactor: exception handling

--- src/main/java/com/project/musinsa/global/exception/DuplicationDataException.java
+++ src/main/java/com/project/musinsa/global/exception/DuplicationDataException.java
@@ -1,6 +1,12 @@
 package com.project.musinsa.global.exception;
 
+import com.project.musinsa.global.enums.Errors;
+import lombok.Getter;
+
+@Getter
 public class DuplicationDataException extends RuntimeException {
+    private final Errors errors = Errors.DUPLICATION_DATA;
+
     public DuplicationDataException(String message) {
         super(message);
     }
--- src/main/java/com/project/musinsa/global/exception/EntityNotFoundException.java
+++ src/main/java/com/project/musinsa/global/exception/EntityNotFoundException.java
@@ -1,6 +1,11 @@
 package com.project.musinsa.global.exception;
 
+import com.project.musinsa.global.enums.Errors;
+import lombok.Getter;
+
+@Getter
 public class EntityNotFoundException extends RuntimeException {
+    private final Errors errors = Errors.ENTITY_NOT_FOUND;
     public EntityNotFoundException(String message) {
         super(message);
     }
