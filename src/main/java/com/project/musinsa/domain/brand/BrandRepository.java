65f5f1b772e3272f49c380ca0dbf66a84197e583
dkf4929 <sng4929@naver.com>
2024-07-13 16:26:30 +0900

    chore: 테스트용 메소드 추가

--- src/main/java/com/project/musinsa/domain/brand/BrandRepository.java
+++ src/main/java/com/project/musinsa/domain/brand/BrandRepository.java
@@ -2,5 +2,8 @@ package com.project.musinsa.domain.brand;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BrandRepository extends JpaRepository<Brand, Long> {
+    Optional<Brand> findByName(String name);
 }
