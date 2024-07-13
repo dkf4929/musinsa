c64b17af0939829940d12a48a2023f1cd2a6fabf
dkf4929 <sng4929@naver.com>
2024-07-13 16:25:57 +0900

    feat: 상품 조회 API 개발

--- src/main/java/com/project/musinsa/domain/item/ItemController.java
+++ src/main/java/com/project/musinsa/domain/item/ItemController.java
@@ -1,6 +1,7 @@
 package com.project.musinsa.domain.item;
 
-import com.project.musinsa.domain.brand.dto.BrandUpdateRequestDto;
+import com.project.musinsa.domain.brand.dto.BrandResponseDto;
+import com.project.musinsa.domain.item.dto.ItemResponseDto;
 import com.project.musinsa.domain.item.dto.ItemSaveRequestDto;
 import com.project.musinsa.domain.item.dto.ItemUpdateRequestDto;
 import io.swagger.v3.oas.annotations.Operation;
@@ -15,12 +16,71 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Tag(name = "ITEM REST API", description = "상품 API")
 @RestController
 @RequestMapping("/musinsa/api/item")
 @RequiredArgsConstructor
 public class ItemController {
     private final ItemService itemService;
+
+    @GetMapping
+    @Operation(summary = "상품 조회", description = "모든 상품을 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "상품이 조회되었습니다.", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = Long.class))),
+            @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(example = "{ \"message\": \"서버 오류 발생.\" }")))
+    })
+    public List<ItemResponseDto> findItemAll() {
+        List<Item> items = itemService.findItemAll();
+
+        // entity to dto
+        return items.stream().map(item -> {
+                    return ItemResponseDto.builder()
+                            .id(item.getId())
+                            .category(item.getItemCategory().getName())
+                            .brand(BrandResponseDto.builder()
+                                    .brandName(item.getBrand().getName())
+                                    .id(item.getBrand().getId())
+                                    .build())
+                            .price(item.getPrice())
+                            .build();
+                })
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{itemId}")
+    @Operation(summary = "상품 조회", description = "특정 상품을 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "상품이 조회되었습니다.", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = Long.class))),
+            @ApiResponse(responseCode = "400", description = "조회 실패", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(example = "{ \"message\": \"유효한 ID가 아닙니다.\" }"))),
+            @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(example = "{ \"message\": \"서버 오류 발생.\" }")))
+    })
+    public ItemResponseDto findItem(@PathVariable Long itemId) {
+        Item item = itemService.findItem(itemId);
+
+        // entity to dto
+        return ItemResponseDto.builder()
+                .id(item.getId())
+                .category(item.getItemCategory().getName())
+                .brand(BrandResponseDto.builder()
+                        .brandName(item.getBrand().getName())
+                        .id(item.getBrand().getId())
+                        .build())
+                .price(item.getPrice())
+                .build();
+    }
 
     // 아이템 저장
     @Operation(summary = "상품 추가", description = "새로운 상품을 추가합니다.")
@@ -37,7 +97,7 @@ public class ItemController {
     })
     @PostMapping
     public ResponseEntity<Long> saveItem(@Valid @RequestBody ItemSaveRequestDto saveRequestDto) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(itemService.saveItem(saveRequestDto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(itemService.saveItem(saveRequestDto).getId());
     }
 
     @Operation(summary = "상품 수정", description = "상품 정보를 수정합니다.")
--- src/main/java/com/project/musinsa/domain/item/ItemService.java
+++ src/main/java/com/project/musinsa/domain/item/ItemService.java
@@ -11,9 +11,13 @@ import com.project.musinsa.global.exception.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ItemService {
     private final ItemRepository itemRepository;
     private final BrandRepository brandRepository;
@@ -23,15 +27,27 @@ public class ItemService {
                 .orElseThrow(() -> new EntityNotFoundException("해당 상품을 찾을 수 없습니다."));
     }
 
-    public Long saveItem(ItemSaveRequestDto requestDto) {
+    // 상품 조회 ALL
+    public List<Item> findItemAll() {
+        return itemRepository.findAll();
+    }
+
+    // ID로 상품 검색
+    public Item findItem(Long itemId) {
+        return findById(itemId);
+    }
+
+    // 상품 저장
+    public Item saveItem(ItemSaveRequestDto requestDto) {
+        // 브랜드 없을 시, EXCEPTION
         Brand brand = brandRepository.findById(requestDto.getBrandId())
                 .orElseThrow(() -> new EntityNotFoundException("해당 브랜드를 찾을 수 없습니다."));
 
-        Item savedItem = itemRepository.save(Item.create(requestDto, brand));
-
-        return savedItem.getId();
+        // 저장
+        return itemRepository.save(Item.create(requestDto, brand));
     }
 
+    // 상품 수정
     public void updateItem(ItemUpdateRequestDto requestDto) {
         Brand brand = brandRepository.findById(requestDto.getBrandId())
                 .orElseThrow(() -> new EntityNotFoundException("해당 브랜드를 찾을 수 없습니다."));
@@ -41,6 +57,7 @@ public class ItemService {
         item.updateItem(requestDto, brand);
     }
 
+    // 상품 삭제
     public void deleteItem(Long itemId) {
         Item findEntity = findById(itemId);
 
